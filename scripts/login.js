c4c5ca5bc7e90a6f7b2de8bf7d8a7fc51c394d20
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-03-07 19:08:24 +0600

    Login page functionality added

---
+++ scripts/login.js
@@ -0,0 +1,15 @@
+
+function getInputValue(){
+    const userName = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if(userName !== "admin"){
+        alert("Invalid Username")
+    }
+    if(password !== "admin123"){
+        alert("Invalid Password")
+    }
+
+    // window.location.href = "https://www.example.com";
+    window.location.assign("./home.html");
+}
